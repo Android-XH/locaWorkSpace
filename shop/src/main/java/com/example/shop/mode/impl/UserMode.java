d8631263952b80c73201e3b37e0fbd70c2e8e0c9
xuhao <xuhao.0103@163.com>
2019-12-06 16:53:31 +0800

    优化编译

--- shop/src/main/java/com/example/shop/mode/impl/UserMode.java
+++ shop/src/main/java/com/example/shop/mode/impl/UserMode.java
@@ -1,10 +1,12 @@
 package com.example.shop.mode.impl;
 
+import com.example.shop.api.param.UserParam;
 import com.example.shop.mode.IUserMode;
+import com.example.worktools.model.CallBack;
 
 public class UserMode extends BaseAppMode implements IUserMode {
     @Override
-    public void login() {
+    public void login(UserParam userParam, CallBack callBack) {
 
     }
 }
